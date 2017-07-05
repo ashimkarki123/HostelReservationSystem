0fac59a4d473cd09b7cd020931db0158a9b17f75
ashimkarki123 <lashimkarki@gmail.com>
2017-07-05 23:30:42 +0545

    Add files via upload

---
+++ admin_index.php
@@ -0,0 +1,42 @@
+
+<html>
+<head>
+    <link href="css/homepage.css" type="text/css" rel="Stylesheet">
+    <link href="css/links.css" type="text/css" rel="Stylesheet">
+    <title>Hostel Reservation System</title>
+<body>
+<div class="main">
+    <div class="header">
+        <div class="logo">
+
+        </div>
+
+        <div class="title">
+            <h1 class="lucy">Hostel Reservation System</h1>
+        </div>
+
+        <div class="activity">
+
+            <a href="view/logout.php">logout</a>
+        </div>
+    </div>
+
+    <div id="menu">
+
+        <ul>
+            <li><a class="active" href="index.php">Home</a></li>
+            <li><a href="view/room.php">Room</a></li>
+            <li><a href="view/admin_profile.php">Profile</a></li>
+            <li><a href="view/admin_view_booking.php">View Booking</a></li>
+
+        </ul>
+    </div>
+
+    <div class="content">
+
+        hello this is ashim
+    </div>
+
+</body>
+</head>
+</html>
